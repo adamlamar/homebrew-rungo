bbaa3c5825332682197bc10a07610d7268c16fd2
Adam Lamar <adamlamar@users.noreply.github.com>
2018-05-17 22:49:56 -0600

    Update to release 0.0.3 (#2)

--- Formula/rungo.rb
+++ Formula/rungo.rb
@@ -1,8 +1,8 @@
 class Rungo < Formula
   desc "Simple shim to run a specific version of Go."
   homepage "https://github.com/adamlamar/rungo"
-  url "https://github.com/adamlamar/rungo/releases/download/0.0.2/rungo-0.0.2-osx.tar.gz"
-  sha256 "1cdd3fc16c0a8f9abcd44c2dd98fafc5f747d13429234d0f8e37b8d5cc8c5f49"
+  url "https://github.com/adamlamar/rungo/releases/download/0.0.3/rungo-0.0.3-osx.tar.gz"
+  sha256 "ce251a5e702e17b3ab1648d962a52fd4bc55a77ab7eb0a14e861672c97c9e181"
 
   def install
     bin.install 'rungo'
